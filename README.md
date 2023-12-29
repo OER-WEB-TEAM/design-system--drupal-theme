446b09b77ddec697bb158acadf316e2cbfa7b6fc
AlexVanK <56647386+AlexVanK@users.noreply.github.com>
2023-12-29 11:32:43 -0500

    Branching out for subtheme boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# drupal-subtheme--nihod
+# NIH OD 5 - Subtheme theme
+
+[Bootstrap 5](https://www.drupal.org/project/bootstrap5) subtheme.
+
+## Development.
+
+### CSS compilation.
+
+Prerequisites: install [sass](https://sass-lang.com/install).
+
+To compile, run from subtheme directory: `sass scss/style.scss css/style.css && sass scss/ck5style.scss css/ck5style.css`
--- js/scripts.js
+++ js/scripts.js
@@ -1,154 +1,40 @@
+// For more information about this wrapper, see
+// https://www.drupal.org/docs/drupal-apis/javascript-api/javascript-api-overview
 (function (Drupal) {
 
-  Drupal.behaviors.jsvalidate = {
-    attach: function () {
-      once("helper__jsvalidate", "[data-jsvalidate]", document.body).forEach(
-        (input) => {
-          input.addEventListener("blur", (e) => {
-            const { target } = e;
+    // Organize your website scripting into Drupal Behaviors
+    // @see: https://www.drupal.org/docs/drupal-apis/javascript-api/javascript-api-overview#s-example-using-drupalbehaviors
 
-            // Skips validation to fields that are empty and not required
-            if (!target.value && !e.target.hasAttribute("required")) return;
+    // Dedicate one behavior per alike-functionalities.
+    // A Drupal Behavior is an advanced API of the famous jQuery's $(document).ready() 
+    // jQuery is deprecated !!STOP USING IT!!
 
-            // Clean old errors
-            input
-              .closest("label")
-              .querySelectorAll(".field-error")
-              .forEach((msg) => msg.remove());
+    Drupal.behaviors.maincontent = {
+        attach: function (context, setting) {
 
-            // Write Error
-            var error_msg = `<div class="field-error text-danger"><b>${target.validationMessage}</b></div>`;
+            // ---
+            // Log the 'context', and 'setting' objects to see their properties
+            // this will help you restrict the behavior where you need it.
+            // ---
 
-            if (!target.reportValidity()) {
-              target
-                .closest("label [data-jsvalidate]")
-                .insertAdjacentHTML("beforebegin", error_msg);
-              target.setAttribute("data-jsvalidate", "error");
-            } else target.setAttribute("data-jsvalidate", "success");
-          });
+            // Example 1 - Run behavior only on pages that contain article element
+            if (!context.querySelector("article")) return;
+
+            // Example 2 - Run behavior only on node 1
+            if (setting.path.currentPath !== "node/1") return;
+
+            // --
+            // Once you've got your restrictions set up, you need to make sure 
+            // that your behavior runs only one time on a node/node-list
+            // @see: https://www.npmjs.com/package/@drupal/once
+            // --
+
+            // Example 1 - Log "Hello World" for each article on the page
+            once("behavior__greetings", "article", document.body).forEach(() => console.log("Hello World"));
+
+            // ---
+            // Note that this behavior actually works, if every condition above is met
+            // ---
         }
-      );
-    },
-  };
-
-  Drupal.behaviors.formjsvalidate = {
-    attach: function () {
-      once("helper__jsvalidate", "form[novalidate]", document.body).forEach(
-        (form) => {
-          form.addEventListener("submit", (e) => {
-            e.preventDefault();
-
-            // Clean old errors
-            form
-              .querySelectorAll(".field-error")
-              .forEach((msg) => msg.remove());
-
-            var badfields = form.querySelectorAll(":invalid");
-            if (!badfields.length) form.submit();
-
-            badfields.forEach((field) => {
-              // Write Error
-              var error_mgs = `<div class="field-error text-danger"><b>${field.validationMessage}</b></div>`;
-              field.insertAdjacentHTML("beforebegin", error_mgs);
-              field.checkValidity();
-            });
-          });
-        }
-      );
-    },
-  };
-
-  Drupal.behaviors.dismissibleAlerts = {
-    attach: function () {
-      once(
-        "helper__dismissiblealerts",
-        "[data-bs-dismiss='alert']",
-        document.body
-      ).forEach((closeButton) => {
-        const alert = closeButton.closest("[role='alert']");
-        const id = "alertdismiss-" + alert.getAttribute("id");
-
-        // First check if alert has been dismissed in the past
-        if (localStorage.getItem(id) || sessionStorage.getItem(id))
-          alert.remove();
-
-        // Then have browser remember new dismissed alerts
-        closeButton.addEventListener("click", (e) => {
-          if (e.target.getAttribute("data-dismiss-for") === "good")
-            localStorage.setItem(id, true);
-          else sessionStorage.setItem(id, true);
-        });
-      });
-    },
-  };
-
-  Drupal.behaviors.comboBox = {
-    attach: function () {
-      once("helper__combobox", ".combobox", document.body).forEach((cmbx) => {
-        const showlist = (_) => {
-          cmbx.firstElementChild.style.outline = _
-            ? "5px solid var(--bs-info)"
-            : "";
-          if (_) cmbx.lastElementChild.classList.remove("d-none");
-          else cmbx.lastElementChild.classList.add("d-none");
-        };
-
-        const filterlist = (a, b) => {
-          a.forEach((_) => {
-            var exp = _.innerText.toLowerCase().includes(b.toLowerCase());
-            _.style.display = exp ? "block" : "none";
-          });
-        };
-
-        document.body.addEventListener("click", (e) =>
-          showlist(cmbx.contains(e.target))
-        );
-        document.body.addEventListener("keyup", (e) => {
-          if (e.key == "Escape") showlist(false);
-        });
-
-        // Handles options filtering
-        const input = cmbx.querySelector("input");
-        const options = cmbx.querySelectorAll("a");
-        const clear = cmbx.querySelector("button i:first-child");
-        input.addEventListener("keyup", (e) => {
-          if (e.target.value.length) clear.classList.remove("d-none");
-          else clear.classList.add("d-none");
-
-          filterlist(options, e.target.value);
-        });
-
-        // Handles clearing the input
-        clear.addEventListener("click", (e) => {
-          input.value = "";
-          filterlist(options, "");
-        });
-
-        // Handles Options
-        options.forEach((option) => {
-          option.addEventListener("click", (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            input.value = e.target.innerText;
-            showlist(false);
-          });
-        });
-      });
-    },
-  };
-
-  Drupal.behaviors.anchorLink = {
-    attach: function (context) {
-      if (context.querySelector("article") == null) return;
-      // Produce an anchor icon for all the headings with class ".anchor"
-      once("helper__anchorLink", ".anchor", document.body).forEach((anchorElement) => {
-        let innerText = anchorElement.innerText;
-        let anchor = innerText.replace(/\s+/g, '-').toLowerCase();
-        anchorElement.setAttribute("id", anchor)
-
-        anchorElement.innerHTML += `<a href="#${anchor}">#</a>`;
-      });
-    },
-  };
-
+    }
 })(Drupal);
